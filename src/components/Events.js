1ac4fabf0c1aa3831ec2f8fd376421b878aadb47
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-02 10:17:09 -0300

    Trabalhando com função e jsx fora do return

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,6 +3,15 @@ export default function Events () {
     console.log(event)
     console.log('Você ativou o Evento')
   }
+
+  const renderSomething = (x) => {
+    if (x) {
+        return <h1>Se x é true retorno isso</h1>;
+    } else {
+        return <h4> Se x é false eu renderizo isso!</h4>
+    }
+  };
+
   return (
     <div>
       <div>
@@ -11,6 +20,9 @@ export default function Events () {
       <div>
         <button onClick={() => console.log("Clicou em outro evento")}>Clique aqui Também!</button>
       </div>
+
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   )
 }
