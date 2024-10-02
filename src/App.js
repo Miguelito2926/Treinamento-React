fb031823bec31e2767f72e7e3a208c68d85a8c7d
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-02 15:08:37 -0300

    Trabalhando com manipulações de imagem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import Donald from "./assets/donald.png";
 
 function App() {
   return (
     <div className="App">
       <h1>Bem-vindo! Fundamentos 2 intermediário,</h1>
-      <p>Teste</p>
+     
+     {/*Imagem em public */}
+     <div>
+      <img src="botafogo.png" alt="Escudo do Fogão" />
+     </div>
+
+      {/*Imagem em asset */}
+     <div>
+      <img src={Donald} alt="Pato" />
+     </div>
     </div>
   );
 }
