a8a3d64304567a9429553920c858d9de476c9620
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-01 23:48:28 -0300

    Mais um exemplo de evento simples

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,12 +1,16 @@
-export default function Events() {
-
-    const meuEvento = (event) => {
-        console.log(event);
-        console.log("Você ativou o Evento");
-    };
-    return(
-        <div>
-            <button onClick={meuEvento}>Clique aqui!</button>
-        </div>
-    )
+export default function Events () {
+  const handleMeuEvento = event => {
+    console.log(event)
+    console.log('Você ativou o Evento')
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={handleMeuEvento}>Clique aqui!</button>
+      </div>
+      <div>
+        <button onClick={() => console.log("Clicou em outro evento")}>Clique aqui Também!</button>
+      </div>
+    </div>
+  )
 }
