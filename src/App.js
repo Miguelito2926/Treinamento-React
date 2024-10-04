d1e0b8f76967d36b30e9d837f4b8d3906b23222a
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-04 15:06:15 -0300

    Reenderização de listas em componentes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,29 @@ import ManageData from './components/ManageData'
 import ShowUserName from './components/ShowUserName'
 
 function App () {
+  const cars = [
+    { id: 1, model: 'Fiat', color: 'Amarelo', km: 0, newCar: true },
+    { id: 2, model: 'Ford', color: 'Azul', km: 0, newCar: true },
+    { id: 3, model: 'VW', color: 'Vermelho', km: 1000, newCar: false },
+    { id: 4, model: 'GM', color: 'Rosa', km: 0, km: 0, newCar: true },
+    { id: 5, model: 'Honda', color: 'Branco', km: 20000, newCar: false },
+    { id: 6, model: 'BYD', color: 'Cinza', km: 50000, newCar: false },
+    { id: 7, model: 'Cherry', color: 'Verde', km: 0, newCar: true }
+  ]
+
   return (
     <div className='App'>
-      <h1>Bem-vindo! Fundamentos 2 intermediário</h1>   
+      {/*Loop em array de objetos, usando iteração do componente*/}
+      {cars.map(car => {
+        <CarDetails
+          model={car.model}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      })}
+
+      <h1>Bem-vindo! Fundamentos 2 intermediário</h1>
 
       {/*Imagem em public */}
       <div>
@@ -20,14 +40,18 @@ function App () {
       <div>
         <img src={Donald} alt='Pato' />
       </div>
-      <ManageData/>
-      <LisRender/>   
-      <ConditionalRender/>   
+      <ManageData />
+      <LisRender />
+      <ConditionalRender />
       {/*Props */}
-      <ShowUserName name="Ed"/>
+      <ShowUserName name='Ed' />
 
       {/*Destrucruring*/}
-      <CarDetails model="FIAT" km={80000} color="Prata"/>
+      <CarDetails model='FIAT' km={80000} color='Prata' newCar={false} />
+
+      {/*Reaproveitando componentes */}
+      <CarDetails model='FORD' km={10000} color='Preto' newCar={false} />
+      <CarDetails model='HONDA' km={0} color='Azul' newCar={true} />
     </div>
   )
 }
--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -1,13 +1,14 @@
 //Trabalhando com Destructuring de props*
-export default function CarDetails({model, km, color}) {
+export default function CarDetails({model, km, color, newCar}) {
   return (
     <div>
         <h2>Detalhes do carro</h2>
         <ul>
-            <li>Marca: {model}</li>
+            <li>Marca: <strong>{model}</strong></li>
             <li>KM: {km}</li>
             <li>Cor: {color}</li>
         </ul>
+        {newCar && <p>Este carro é zero!</p>}
     </div>
   )
 }
