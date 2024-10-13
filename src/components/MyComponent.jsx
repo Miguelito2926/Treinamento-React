6dfed2e0780ab04c83ad0ec184d5ab64d374fd26
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-12 22:35:27 -0300

    Trabalhando css inline

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -6,6 +6,7 @@ export default function MyComponent() {
     <div>
         <h1>Css de Compomente</h1>
         <p>Parágrafo do Compomente</p>
+        <p style={{color:'green', padding:'10px', borderTop:'2px solid red'}}>Parágrafo estilizado com css inline</p>
         <p className='my-component-paragrafo'>Este Também é Parágrafo do Compomente estilizado com className </p>
     </div>
   )
