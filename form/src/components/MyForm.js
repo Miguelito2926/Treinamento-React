faf7e16aeaad80568661d66e3f71f1970cdf36f4
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-10-14 21:13:07 -0300

    avançando em formularios

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import './MyForm.css';
 
 export default function MyForm() {
+
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (event) => {
+        setName(event.target.value)
+    }
+
     return(
         <div>
 
@@ -8,14 +18,18 @@ export default function MyForm() {
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite seu nome." />
+                    <input type="text" name="name" placeholder="Digite seu nome." onChange={handleName}/>
                 </div>
 
                    {/* 2 - label envolvendo input*/}
                    <label>
                     <span>E-mail:</span>
-                    <input type="email" name='email' placeholder='Digite seu e-mail.'/>
-                   </label>
+                    <input 
+                    type="email"
+                    name='email' 
+                    placeholder='Digite seu e-mail.'
+                    onChange={(event) => setEmail(event.target.value)}/>
+                   </label>                   
                    <input type="submit" value='Enviar' />
             </form>
         </div>
